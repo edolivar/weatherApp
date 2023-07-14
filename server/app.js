71776dff236a855d93bec56123282e3d774342d9
Edwin Olivares <edwin@Edwins-MacBook-Pro.local>
2023-07-14 12:54:45 -0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,7 @@
+import express from "express"
+
+const app = express()
+const PORT = process.env.PORT || 8080
+app.get('/', (req, res) => { res.send('Hello World') })
+
+app.listen(PORT, () => { console.log("Listening on Port:" + PORT) })
